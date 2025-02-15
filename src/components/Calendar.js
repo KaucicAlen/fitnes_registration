a3fea3caab4fd0fa9792343e1e28fbb8745fef46
Alen Kaucic <alen.kaucic@student.um.si>
2025-02-15 23:05:14 +0100

    removed hours

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -8,9 +8,8 @@ const Calendar = ({ token }) => {
     const [selectedDay, setSelectedDay] = useState(currentDay.charAt(0).toUpperCase() + currentDay.slice(1));
     const daysOfWeek = ["Ponedeljek", "Torek", "Sreda", "Cetrtek", "Petek", "Sobota", "Nedelja"];
     const hours = [
-        "00:00", "1:00", "2:00", "3:00", "4:00", "5:00",
-        "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", 
-        "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00", "24:00"
+        "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00",
+        "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00"
     ];
 
     // Fetch reservations when the component mounts
@@ -100,7 +99,7 @@ const Calendar = ({ token }) => {
                             >
                                 {day}
                             </h3>
-    
+
                             {/* Only display reservations for the selected day */}
                             {selectedDay === day && (
                                 <div className="reservation-grid">
@@ -112,7 +111,7 @@ const Calendar = ({ token }) => {
                                         const reservedBy = reservation ? reservation.username : "";
                                         const loggedInUser = localStorage.getItem("username");
                                         const isUserOwner = reservation && reservedBy === loggedInUser;
-    
+
                                         return (
                                             <div
                                                 key={index}
@@ -140,7 +139,7 @@ const Calendar = ({ token }) => {
             )}
         </div>
     );
-    
+
 };
 
 export default Calendar;
