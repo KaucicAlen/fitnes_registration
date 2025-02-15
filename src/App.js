70f93d660545a2b1f470f224fd8f8a23bf2414e8
Alen Kaucic <alen.kaucic@student.um.si>
2025-02-15 22:31:48 +0100

    deploy fix

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <Router>
       <div className="container">
-        <Header token={token} username={username} handleLogout={handleLogout} />
+        <Header token={token} username={username} />
 
         <Routes>
           <Route path="/register" element={<Register />} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-const Header = ({ token, username, handleLogout }) => {
+const Header = ({ token, username }) => {
   
   const navigate = useNavigate();
 
